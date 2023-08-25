4d4f79d20ea169e5cb6f4a2d265d303c4f73f1e7
danyagomezcantu <111596636+danyagomezcantu@users.noreply.github.com>
2023-08-24 23:18:27 -0600

    Update P1A2-Boton-Freq.ino

--- P1A2-Boton-Freq/P1A2-Boton-Freq.ino
+++ P1A2-Boton-Freq/P1A2-Boton-Freq.ino
@@ -1,31 +1,31 @@
-#define BOTON 2
-#define LED 15
+#define BOTON 2 //Define el GPIO2 como "BOTON")
+#define LED 15 //Define el GPIO15 como "LED")
 
-bool estadoBTN;
+bool estadoBTN; //Declara la variable "estadoBTN" como booleano (binario)
 
 void setup() {
-  pinMode(BOTON,INPUT);
-  pinMode(LED,OUTPUT);
-  Serial.begin(115200);
+  pinMode(BOTON,INPUT); //Configura el botón como una entrada
+  pinMode(LED,OUTPUT); //Configura el led como una salida
+  Serial.begin(115200); //Establece comunicación serial a 115,200 baudios
 
 }
 
 void loop() {
-  estadoBTN = digitalRead(BOTON);
+  estadoBTN = digitalRead(BOTON); //Designa a la variable "estadoBTN" la lectura del botón
 
-  if (estadoBTN == true) {
-    Serial.println("Presionado");
-    digitalWrite(LED,HIGH);
-    delay(125);
-    digitalWrite(LED,LOW);
-    delay(125);
+  if (estadoBTN == true) { //Si el botón está presionado
+    Serial.println("Presionado"); //Imprime "Presionado"
+    digitalWrite(LED,HIGH); //Enciende el LED
+    delay(125); //Espera un cuarto de segundo
+    digitalWrite(LED,LOW); //Apaga el LED
+    delay(125); //Espera un cuarto de segundo
   }
 
-  else{
-    Serial.println("Sin presionar");
-    digitalWrite(LED,HIGH);
-    delay(500);
-    digitalWrite(LED,LOW);
-    delay(500);
+  else{ //Si el botón no está presionado
+    Serial.println("Sin presionar"); //Imprime "Sin presionar"
+    digitalWrite(LED,HIGH); //Enciende el LED
+    delay(500); //Espera medio segundo
+    digitalWrite(LED,LOW); //Apaga el LED
+    delay(500); //Espera medio segundo
   }
 }
