2499dba3ee04924e83e4131463f976b4051e801f
danyagomezcantu <111596636+danyagomezcantu@users.noreply.github.com>
2023-08-24 23:17:09 -0600

    Update P1A1-Blink.ino

--- P1A1-Blink/P1A1-Blink.ino
+++ P1A1-Blink/P1A1-Blink.ino
@@ -1,12 +1,12 @@
-#define LED 15 //Definir el GPIO15 como "LED"
+#define LED 15 //Define el GPIO15 como "LED"
 
 void setup() {
-  pinMode(LED,OUTPUT); //Configurar el LED como una salida
+  pinMode(LED,OUTPUT); //Configura el LED como una salida
 }
 
 void loop() {
   digitalWrite(LED,HIGH); //Manda una señal para encender el LED
-  delay(500); //Esperar medio segundo
-  digitalWrite(LED,LOW); //Apagar el LED
-  delay(500); //Esperar medio segundo
+  delay(500); //Espera medio segundo
+  digitalWrite(LED,LOW); //Apaga el LED
+  delay(500); //Espera medio segundo
 }
