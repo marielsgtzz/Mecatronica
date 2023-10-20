6e77446da55207616e2f8fe570718dc2d68dfc9a
danyagomezcantu <111596636+danyagomezcantu@users.noreply.github.com>
2023-10-20 14:32:19 -0600

    Update P6A3-MedicionPosVel.ino

--- P_Mecatronica/Practica06/P6A3-MedicionPosVel/P6A3-MedicionPosVel.ino
+++ P_Mecatronica/Practica06/P6A3-MedicionPosVel/P6A3-MedicionPosVel.ino
@@ -1,7 +1,5 @@
 // P6A3-MedicionPosVel
 
-#include <Arduino.h>
-
 // Definición de pines
 #define EnM 21  // Pin para habilitar/deshabilitar el motor
 #define MA 23   // Pin de control A para el puente H
@@ -15,9 +13,18 @@ int resolution = 12; // Resolución de la señal PWM
 volatile long pulses = 0;  // Cuenta de pulsos del encoder
 const int PPR = 64;      // Pulsos por revolución del eje de salida del reductor
 
-int factor = 1;
-int tiempo = 20;
-int buttonstate;
+// Variables para el cálculo de velocidad
+float angulo;
+float velocidad_rads;
+float velocidad_rps;
+float velocidad_rpm;
+
+
+// Medición del tiempo
+unsigned long tiempoAnterior = micros(); // Variable para almacenar el tiempo previo en microsegundos
+unsigned long tiempoActual = 0; // Se actualiza al calcular la velocidad
+unsigned long tiempoCambioVelocidad = 3000000; // 3 segundos en microsegundos
+unsigned long tiempoCalculo = 100000; // 100 milisegundos en microsegundos
 
 // Función que se llama cada vez que el pin A del encoder tiene un flanco ascendente
 void IRAM_ATTR PulsesCounter() {
@@ -28,43 +35,105 @@ void IRAM_ATTR PulsesCounter() {
   }
 }
 
-unsigned long prevTime = micros(); // Variable para almacenar el tiempo previo en microsegundos
+void setup() {
+  // Configuración de pines de salida
+  pinMode(EnM, OUTPUT);
+  pinMode(MA, OUTPUT);
+  pinMode(MB, OUTPUT);
+
+  // Configuración de pines de entrada para el encoder
+  pinMode(EncA, INPUT);
+  pinMode(EncB, INPUT);
+
+  // Interrupción para contar pulsos del encoder
+  attachInterrupt(digitalPinToInterrupt(EncA), PulsesCounter, RISING);
+
+  // Configura el PWM
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(EnM, channel);
+  
+  // Iniciar la comunicación serial
+  Serial.begin(115200);
+}
+
+int etapa = 1;
+
+void loop() {
+  tiempoActual = micros();
+  
+  // Cambio de velocidad cada 3 segundos
+  if (tiempoActual - tiempoAnterior >= tiempoCambioVelocidad) {
+    switch(etapa) {
+      case 1:
+        // Etapa 1: Girar al 10%
+        enviarComandoMotor(25);
+        break;
+      case 2:
+        // Etapa 2: Girar al 30%
+        enviarComandoMotor(76);
+        break;
+      case 3:
+        // Etapa 3: Girar al 50%
+        enviarComandoMotor(128);
+        break;
+      case 4:
+        // Etapa 4: Girar al 100%
+        enviarComandoMotor(255);
+        break;
+      case 5:
+        // Etapa 5: Girar al 0%
+        enviarComandoMotor(0);
+        break;
+    }
+
+    etapa++; // Avanzar a la siguiente etapa
+
+    if(etapa > 5) {
+      etapa = 1; // Reiniciar si ya se completaron todas las etapas
+    }
+
+    tiempoAnterior = tiempoActual; // Actualiza el tiempo previo
+  }
+
+  // Cálculo de velocidad cada 100 milisegundos
+  if (tiempoActual - tiempoAnterior >= tiempoCalculo) {
+    calcularVelocidad();
+    imprimirResultados();
+    tiempoAnterior = tiempoActual; // Actualiza el tiempo previo
+  }
+}
 
 void enviarComandoMotor(int velocidad) {
-  analogWrite(MA, velocidad);
-  analogWrite(MB, 0);
+  analogWrite(EnM, velocidad);
+  // Giro levógiro
+  digitalWrite(MA, LOW);
+  digitalWrite(MB, HIGH);
 }
 
 void calcularVelocidad() {
-  unsigned long currentTime = micros();
+  tiempoActual = micros();
   long currentPulses = pulses;
 
   long deltaPulses = currentPulses - pulses;
-  unsigned long deltaTime = currentTime - prevTime;
+  unsigned long deltaTime = tiempoActual - tiempoAnterior;
 
   float deltaTimeSeconds = float(deltaTime) / 1000000.0;
 
-  float velocidad_rads = (deltaPulses * 2 * PI) / (PPR * deltaTimeSeconds);
-
-  float velocidad_rps = velocidad_rads / (2 * PI);
-  float velocidad_rpm = velocidad_rps * 60;
-
-  pulses = currentPulses; // Actualiza el valor de pulsos
-  prevTime = currentTime; // Actualiza el tiempo previo
-}
-
-void imprimirResultados() {
   // Calcular ángulo en grados
-  float angulo = (pulses % PPR) * 360.0 / PPR;
+  angulo = (pulses % PPR) * 360.0 / PPR;
 
   // Calcular velocidad angular en radianes por segundo
-  float velocidad_rads = (pulses * 2 * PI) / (PPR * (micros() - prevTime) / 1000000.0);
+  velocidad_rads = (pulses * 2 * PI) / (PPR * (micros() - tiempoAnterior) / 1000000.0);
 
   // Convertir velocidad a rps y rpm
-  float velocidad_rps = velocidad_rads / (2 * PI);
-  float velocidad_rpm = velocidad_rps * 60;
+  velocidad_rps = velocidad_rads / (2 * PI);
+  velocidad_rpm = velocidad_rps * 60;
 
-  // Imprimir resultados en el monitor serial
+  pulses = currentPulses; // Actualiza el valor de pulsos
+  tiempoAnterior = tiempoActual; // Actualiza el tiempo previo
+}
+
+void imprimirResultados() {  // Imprimir resultados en el monitor serial
   Serial.print("Pulsos: ");
   Serial.print(pulses);
   Serial.print(" --- Ángulo: ");
@@ -76,49 +145,4 @@ void imprimirResultados() {
   Serial.print(" [rpm] --- ");
   Serial.print(velocidad_rads);
   Serial.println(" [rad/s]");
-}
-
-void setup() {
-  pinMode(MA, OUTPUT);
-  pinMode(MB, OUTPUT);
-  pinMode(EncA, INPUT);
-  pinMode(EncB, INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(EncA), PulsesCounter, RISING);
-
-  Serial.begin(115200);
-}
-
-void loop() {
-  
-  unsigned long tiempoActual = micros();
-
-  if (tiempoActual - prevTime >= 100000) {
-    // Etapa 1: Girar al 10%
-    enviarComandoMotor(25);
-    calcularVelocidad();
-    imprimirResultados();
-
-    // Etapa 2: Girar al 30%
-    enviarComandoMotor(76);
-    calcularVelocidad();
-    imprimirResultados();
-
-    // Etapa 3: Girar al 50%
-    enviarComandoMotor(128);
-    calcularVelocidad();
-    imprimirResultados();
-
-    // Etapa 4: Girar al 100%
-    enviarComandoMotor(255);
-    calcularVelocidad();
-    imprimirResultados();
-
-    // Etapa 5: Girar al 0%
-    enviarComandoMotor(0);
-    calcularVelocidad();
-    imprimirResultados();
-
-    prevTime = tiempoActual; // Actualiza el tiempo previo
-  }
 }
