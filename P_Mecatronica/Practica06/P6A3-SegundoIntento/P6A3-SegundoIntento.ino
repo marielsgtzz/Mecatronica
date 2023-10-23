bafa065900619baaccd267afed98e257338fd311
MarielSoGuZa <marielsgtzz@gmail.com>
2023-10-23 15:41:31 -0600

    Imprimir cada 100ms y mover el motor

--- P_Mecatronica/Practica06/P6A3-SegundoIntento/P6A3-SegundoIntento.ino
+++ P_Mecatronica/Practica06/P6A3-SegundoIntento/P6A3-SegundoIntento.ino
@@ -1,11 +1,11 @@
 //P6A2–Contador de Vueltas
 //enviar una señal al motor para que gire a una velocidad constante máxima
-//en sentido dextrógiro por 3 segundos, se detenga por 2 y gire en 
+//en sentido dextrógiro por 3 segundos, se detenga por 2 y gire en
 //sentido levógiro a velocidad constante máxima durante 3 segundos y enseguida, se quede detenido por 2
 
 //El puente H va a estar leyendo la señal del encoder la cual emite pulsos en ambos sentidos de giro
 
-//El progama cuenta las vueltas que está realizando y muestra en el monitor serial qué tipo de giro es y cuántas vueltas van 
+//El progama cuenta las vueltas que está realizando y muestra en el monitor serial qué tipo de giro es y cuántas vueltas van
 //(Sentido dextrogiro - negativas) (Sentido levogiro - positivas)
 
 // Definición de pines
@@ -14,18 +14,19 @@
 #define MB 22   // Pin de control B para el puente H
 #define EncA 19 // Pin A del encoder
 #define EncB 18 // Pin B del encoder
-
-int channel = 0;     // Canal PWM que se usará
-int freq = 1000;     // Frecuencia de la señal PWM
-int resolution = 12; // Resolución de la señal PWM --> Vel máxima es 4095 
-volatile long pulses = 0;  // Cuenta de pulsos del encoder
-const int PPR = 284;      // Pulsos por revolución del eje de salida del reductor
+int channel = 0;     
+int freq = 1000;     
+int resolution = 12; 
+volatile long pulses = 0;  
+const int PPR = 284;      
 
 int porcentaje = 0;
+unsigned long tiempoAnteriorA = 0;
+unsigned long tiempoAnteriorB = 0;
+unsigned long tiempoActual = 0;
 
-// Función que se llama cada vez que el pin A del encoder tiene un flanco ascendente
 void IRAM_ATTR PulsesCounter() {
-  if(digitalRead(EncB)==HIGH){
+  if (digitalRead(EncB) == HIGH) {
     pulses++;
   } else {
     pulses--;
@@ -33,65 +34,64 @@ void IRAM_ATTR PulsesCounter() {
 }
 
 void setup() {
-  // Configuración de pines de salida
   pinMode(EnM, OUTPUT);
   pinMode(MA, OUTPUT);
   pinMode(MB, OUTPUT);
-
-  // Configuración de pines de entrada para el encoder
   pinMode(EncA, INPUT);
   pinMode(EncB, INPUT);
-
-  // Interrupción para contar pulsos del encoder
   attachInterrupt(digitalPinToInterrupt(EncA), PulsesCounter, RISING);
-
-  // Configura el PWM
   ledcSetup(channel, freq, resolution);
   ledcAttachPin(EnM, channel);
-  
-  // Iniciar la comunicación serial
   Serial.begin(115200);
+  tiempoActual = millis(); // Usa directamente millis() para el tiempo en milisegundos
 }
 
 void loop() {
-  switch(porcentaje){
-    case 0:
-      porcentaje += 10;
-    break;
-    case 10:
-      porcentaje += 20;
-    break;
-    case 30:
-      porcentaje += 20;
-    break;
-    case 50:
-      porcentaje += 50;
-    break;
-    case 100:
-      porcentaje = 0;
-    break;
-    default:
-      Serial.print("Error");
-    break;
+  tiempoActual = millis();
+  // Comprobación para imprimir en el monitor serial cada 100 ms
+  if (tiempoActual - tiempoAnteriorA == 100) {
+    Serial.print("Pulsos:");
+    Serial.print(pulses);
+    Serial.print(" || ");
+    Serial.print("%");
+    Serial.print(porcentaje);
+    Serial.print("\n");
+    tiempoAnteriorA = tiempoActual;
+    
   }
+
+  // Comprobación para cambiar la velocidad del motor cada 3 segundos
+  if (tiempoActual - tiempoAnteriorB == 3000) {
+    switch (porcentaje) {
+      case 0:
+        porcentaje += 10;
+        break;
+      case 10:
+        porcentaje += 20;
+        break;
+      case 30:
+        porcentaje += 20;
+        break;
+      case 50:
+        porcentaje += 50;
+        break;
+      case 100:
+        porcentaje = 0;
+        break;
+      default:
+        Serial.print("Error");
+        break;
+    }
+    tiempoAnteriorB = tiempoActual;
+  }
+
+  moverMotor();
   
+}
+
+void moverMotor() {  
   int porcentajeVel = (4095 * porcentaje) / 100;
-  // Girar a la izquierda durante 3 segundos
   ledcWrite(channel, porcentajeVel);
-  //analogWrite(EnM, 255);  // Velocidad máxima
   digitalWrite(MA, LOW);
   digitalWrite(MB, HIGH);
-  
-
-  Serial.print("Pulsos:");
-  Serial.print(pulses);
-  Serial.print(" || ");
-
-  Serial.print("%");
-  Serial.print(porcentaje);
-  Serial.print("\n");
-
-
-  delay(3000);
-
 }
