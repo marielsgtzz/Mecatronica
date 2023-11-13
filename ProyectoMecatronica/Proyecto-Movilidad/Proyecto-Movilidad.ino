52555770ccdbfaa5c508fbff623c3fb3279a9089
MarielSoGuZa <marielsgtzz@gmail.com>
2023-11-13 16:09:47 -0600

    Inicio Movimiento

---
+++ ProyectoMecatronica/Proyecto-Movilidad/Proyecto-Movilidad.ino
@@ -0,0 +1,126 @@
+//Conexio Bluetooth
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLE) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run 'make menuconfig' to and enable it
+#endif
+
+BluetoothSerial SerailBT;
+char comandoMov;
+
+//Puertos
+#define ENA1 14
+#define ENA2 32
+#define InA1 33 //MA
+#define InB1 25 //MB
+#define InA2 26 //MA
+#define InB2 27 //MB
+// //Conexiones Puente H
+// #define EnMIzq          //Pin para habilitar/deshabilitar el motor izquierdo (el del centro)
+// #define MAIzq 12        // Pin de control A para el puente H del motor izquierdo (primero de la derecha)
+// #define MBIzq 14        // Pin de control B para el puente H del motor izquierdo (tercero de la derecha)
+
+// #define EnMDer          // Pin para habilitar/deshabilitar el motor derecho
+// #define MADer 12        // Pin de control A para el puente H del motor derecho
+// #define MBDer 14        // Pin de control B para el puente H del motor derecho
+
+
+// #define EncAIzq 19      // Pin A del encoder del motor izquierdo
+// #define EncBIzq 18      // Pin B del encoder del motor izquierdo
+
+
+// #define EncADer 19      // Pin A del encoder del motor derecho
+// #define EncBDer 18      // Pin B del encoder del motor derecho
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ENA1, OUTPUT);
+  pinMode(InA1, OUTPUT);
+  pinMode(InB1, OUTPUT);
+  pinMode(ENA2, OUTPUT);
+  pinMode(InA2, OUTPUT);
+  pinMode(InB2, OUTPUT);
+
+  SerialBT.begin("MMP");
+
+}
+
+void loop() {
+  if(Serial.available()){
+    SerialBT.write(Serial.read());
+  }
+
+  if(SerialBT.available()){
+    dato=SerialBT.read();
+    if(dato=='w'){
+      adelante();
+    } else if (dato=='s'){
+      atras();
+    } else if (dato=='a'){
+      izquierda();
+    } else if (dato=='d'){
+      derecha();
+    } else {
+      detenido();
+    }
+  }
+}
+
+void adelante(){
+  digitalWrite(InA1, LOW);
+  digitalWrite(InB1, HIGH);
+
+  digitalWrite(InA2, LOW);
+  digitalWrite(InB2, HIGH);
+
+  Serial.println("ADELANTE");
+
+  delay(1000);
+}
+
+void atras(){
+  digitalWrite(InA1, HIGH);
+  digitalWrite(InB1, LOW);
+
+  digitalWrite(InA2, HIGH);
+  digitalWrite(InB2, LOW);
+
+  Serial.println("ATRAS");
+
+  delay(1000);
+}
+
+void derecha(){
+  digitalWrite(InA1, HIGH);
+  digitalWrite(InB1, LOW);
+
+  digitalWrite(InA2, LOW);
+  digitalWrite(InB2, LOW);
+
+  Serial.println("DERECHA");
+
+  delay(1000);
+}
+
+void izquierda(){
+  digitalWrite(InA1, LOW);
+  digitalWrite(InB1, LOW);
+
+  digitalWrite(InA2, HIGH);
+  digitalWrite(InB2, LOW);
+
+  Serial.println("IZQUIERDA");
+
+  delay(1000);
+}
+
+void detenido(){
+  digitalWrite(InA1, LOW);
+  digitalWrite(InB1, LOW);
+
+  digitalWrite(InA2, LOW);
+  digitalWrite(InB2, LOW);
+
+  Serial.println("ALTO");
+
+  delay(1000);
+}
